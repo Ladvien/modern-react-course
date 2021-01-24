dd8cee764791a63154d7159bec4150c590c1aadd
C. Thomas Brittain <honeysucklehardware@gmail.com>
2021-01-24 10:26:32 -0600

    Added Seasons project

---
+++ seasons/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMessage: '' };
+  }
+
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+          this.setState({
+            lat: position.coords.latitude
+          })
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message }); 
+      }
+    );
+    
+
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage} </div>
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+    return <div>Loading...</div>
+  }
+
+
+
+}
+
+export default App;
