a3e63e24017cc8cc44d1bf6dca764e8480306a96
C. Thomas Brittain <honeysucklehardware@gmail.com>
2021-01-24 14:20:26 -0600

    73 Added message and default props to Spinner

--- seasons/src/Spinner.js
+++ seasons/src/Spinner.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const Spinner = () => {
+const Spinner = (props) => {
   return ( 
     <div className="ui active dimmer">
-      <div className="ui big text loader">Loading...</div>
+      <div className="ui big text loader">{props.message}</div>
     </div>
   )
+}
+
+// ✅ This is how default properties should be set.
+// ❌ Not using OR statements in line. 
+Spinner.defaultProps = {
+    message: 'Loading...'
 }
 
 export default Spinner;
