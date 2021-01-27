c5d03e43196c80d2b712c883420ab55474636323
C. Thomas Brittain <honeysucklehardware@gmail.com>
2021-01-27 03:47:46 -0600

    07 Added axios requests to Unsplash API

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+
+// ❌❌❌
+// Do not use .env variables in production React Apps 
+// they are visible to the user.  The only safe way to 
+// store them is on the backend (e.g., Node server).
+// ❌❌❌
+const access = process.env.REACT_APP_UNSPLASH_ACCESS;
+const secret = process.env.REACT_APP_UNSPLASH_SECRET;
 
 class App extends React.Component {
 
-    onSearchSubmit(term) {
-        console.log(term)
+    async onSearchSubmit(term) {
+        console.log(access);
+        const res = await axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+                query: term
+            },
+            headers:{
+                Authorization: `Client-ID ${access}`
+            }
+        });
     }
 
     render() {
