0439a2b1632df15f4a6460115a6a37ab5c41fdc4
C. Thomas Brittain <honeysucklehardware@gmail.com>
2021-01-30 07:39:40 -0600

    114 Added createRef and image load callback

---
+++ pics/src/components/ImageCard.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+class ImageCard extends Component {
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        console.log(this.imageRef.current.clientHeight);
+    }
+    
+
+    render() {
+        const { urls, alt_description } = this.props.image;
+
+        return (
+            <div>
+                <img ref={this.imageRef}
+                     src={urls.regular} 
+                     alt={alt_description}
+                />
+            </div>
+        )
+    }
+}
+
+export default ImageCard;
