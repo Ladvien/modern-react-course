387ee29b3efce49eabc749e73702bb570fa6b90d
C. Thomas Brittain <honeysucklehardware@gmail.com>
2021-02-02 07:13:33 -0600

    154 added Search widget.

--- widgets/src/App.jsx
+++ widgets/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Accordion from './components/Accordion';
+import Search from './components/Search';
 
 const items = [
     {
@@ -22,7 +23,7 @@ class App extends Component {
     render() { 
         return ( 
             <div>
-                <Accordion items={items} />
+                <Search />
             </div>
          );
     }
---
+++ widgets/src/components/Search.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const Search = () => {
+
+    const [term, setTerm] = useState('');
+
+    return ( 
+        <div className="ui form">
+            <div className="field">
+                <label>Enter Search Term</label>
+                <input input={term} onChange={(e) => setTerm(e.target.value)} className="input" />
+            </div>
+        </div>
+    );
+}
+ 
+export default Search;
