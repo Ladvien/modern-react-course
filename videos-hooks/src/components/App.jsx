34b26419358bd8e70d888fe9514b8dc41942f1a5
C. Thomas Brittain <honeysucklehardware@gmail.com>
2021-02-07 08:24:58 -0600

    211 Refactored videos to use hooks.

---
+++ videos-hooks/src/components/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+import youtube from "../apis/youtube";
+
+const App = () => {
+    const [videos, setVideos] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
+
+    useEffect(() => {
+        onTermSubmit("lady");
+    }, []);
+
+    const onTermSubmit = async (term) => {
+        const res = await youtube.get("/search", {
+            params: {
+                q: term,
+            },
+        });
+
+        setVideos(res.data.items);
+        setSelectedVideo(res.data.items[0]);
+    };
+
+    const onVideoSelect = (video) => {
+        setSelectedVideo(video);
+    };
+
+    return (
+        <div className="ui container">
+            <SearchBar onTermSubmit={onTermSubmit} />
+            <div className="ui grid">
+                <div className="ui row">
+                    <div className="eleven wide column">
+                        <VideoDetail video={selectedVideo} />
+                    </div>
+                    <div className="five wide column">
+                        <VideoList
+                            videos={videos}
+                            onVideoSelect={onVideoSelect}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
