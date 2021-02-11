58d2d24818dddf4847712b75585b887a480e1e36
C. Thomas Brittain <honeysucklehardware@gmail.com>
2021-02-11 16:57:57 -0600

    278 Finished user retrieval

---
+++ blog/src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+import postReducer from "./postReducers";
+import userReducer from "./userReducers";
+
+export default combineReducers({ posts: postReducer, users: userReducer });
